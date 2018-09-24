946d904025279fc5d38527453b965bbfcd2d5b5f
Leonel <Leonel@ESCRITORIO>
2018-09-23 23:40:56 -0300

    23/9

--- src/logic/Main.java
+++ src/logic/Main.java
@@ -1,10 +1,13 @@
 package logic;
 
+import visual.Principal;
+
 public class Main {
 
     
     public static void main(String[] args) {
-        
+        Principal p = new Principal();
+        p.setVisible(true);
     }
     
 }
--- src/logic/PLogic.java
+++ src/logic/PLogic.java
@@ -9,24 +9,30 @@ import java.util.Arrays;
 
 public class PLogic {
     
-    ArrayList lista;
+    ArrayList<Registro> lista;
     File archivo;
-    static final String SEPARATOR = ";";
+    static final String SEPARATOR = ",";
     static final String QUOTE = "\"";
     
     public PLogic(){
-        lista = new ArrayList();
+        lista = new ArrayList<>();
     }
     
     public void cargarArchivo(String ruta){
         archivo = new File(ruta);
         BufferedReader br = null;
+        Registro nuevo;
         try{
             br = new BufferedReader(new FileReader(archivo));
             String line = br.readLine();
             while(line != null){
                 String[] fields = line.split(SEPARATOR);
-                System.out.println(Arrays.toString(fields));
+                nuevo = new Registro(fields[0], fields[1],fields [5], fields[2]);
+                System.out.println(fields[0]);
+                System.out.println(fields[1]);
+                System.out.println(fields[5]);
+                System.out.println(fields[2]);
+                line = br.readLine();
             }
             br.close();
         }catch(IOException ex){
